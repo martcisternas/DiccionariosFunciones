d28299c6ccd1436d94ca8a4e010782e7fae46cf6
martcisternas <mart.cisternasm@duocuc.cl>
2026-06-11 20:03:38 -0400

    Solucion prueba formativa 4

--- Evaluacion formativa 4/Ejercicio1.py
+++ Evaluacion formativa 4/Ejercicio1.py
@@ -1,73 +1,118 @@
-def ingresar_usuario(usuarios):
-    nombre = input("Ingrese nombre del usuario: ").strip()
-    
-    if nombre == "":
-        print("El nombre no puede estar vacio.")
-        return
-    
-    if nombre in usuarios:
-        print("El usuario ya existe.")
-        return
-    
-    sexo = input("Ingrese el sexo del usuario (F / M): ").upper()
+'''usuarios = {
+    "juan" : ["M","qwertyuiop"],
+    "maria" : ["F", "fasdasdf2"]
+}
+#Usaremos este para el ejercicio
+usuario2 = {
+    "daniel":{
+        "sexo":"M",
+        "pass":"1234asdf"
+    },
+    "jorge:{"
+    "sexo":"M",
+    "pass":"4567asdf"
+    }
 
-    if sexo == "F" or sexo == "M":
-        print("Agregado correctamente...")
-    else:
-        print("Solo se permite (F o M).")
-        return    
-    
-    contraseña = (input("Ingrese la contraseña del usuario: ")).strip()
+'''
+#funciones
 
-    if len(contraseña) < 8:
-        print("La contraseña debe tener al menos 8 digitos.")
-        return
-    
-    if contraseña == "":
-        print("La constraseña no puede estar vacia.")
-    
-    for letra in contraseña:
-        if letra.isdigit:
-           return True
+def ingresar_usuario():
+    while True:
+        nombre = input("Ingrese nombre de usuario: ")
+
+        if nombre in usuarios:
+            print("Usuario ya existe, intente con otro nombre.")
         else:
-            print("La contraseña no tiene digitos.")
+            break
+
+    while True:
+        sexo = input("Ingrese sexo(M,F): ").upper()
+
+        if sexo == "M" or sexo == "F":
+            break
+        else:
+            print("El sexo debe ser M o F, Ingrese nuevamente!")
+
+    while True:
+        contraseña = input("Ingrese contraseña: ")
+        
+        if validar_contraseña(contraseña):
+            #print("Contraseña Válida")
+            break
+        else:
+            print("Contraseña inválida, debe tener 8 caracteres entre letras y números")
+
+    #Agregue el usuario con diccionarios.
+    usuarios[nombre] = {
+        "sexo" : sexo,
+        "contraseña" : contraseña
+    }
+    print("Usuario ingresado correctamente!")
+
+def validar_contraseña(contraseña):
+    if len(contraseña) < 8:
+        return False
     
-usuarios = {}
+    if " " in contraseña:
+        return False
+    
+    num = False
+    letra = False
 
+    for caracter in contraseña:
+        if caracter.isdigit():
+            num = True
+        if caracter.isalpha():
+            letra = True
 
+    return num and letra
 
+def buscar_usuario():
+    nombre = input("Ingrese nombre a buscar: ")
+
+    if nombre in usuarios:
+        print("Sexo: ",usuarios[nombre]["sexo"])
+        print(f"Contraseña: {usuarios[nombre]["contraseña"]}")
+    else:
+        print("Nombre no encontrado!")
+
+def eliminar_usuario():
+    nombre = input("Ingrese nombre a buscar: ")
+
+    if nombre in usuarios:
+        del usuarios[nombre]
+        print("Usuario eliminado!")
+    else:
+        print("Nombre no encontrado!")
+
+#parte ppal
+usuarios ={}
 
 while True:
-    print("-----MENU-----")
-    print("1- Ingresar usuario.")
-    print("2- Buscar usuario.")
-    print("3- Eliminar usuario.")
-    print("4- Salir.")
+    print("--- Menu Principal ---")
+    print("1. Ingresar Usuario.")
+    print("2. Buscar Usuario.")
+    print("3. Eliminar Usuario.")
+    print("4. Salir")
 
-    try:
-        op = int(input("Ingrese una opcion: "))
+    while True:
+        try:
+            op = int(input("Ingrese una opción"))
+            break
+        except ValueError:
+            print("Por favor ingrese una opción válida!")
 
-        if op > 4:
-            print("Ingrese una opcion entre 1 y 4.")
-            continue 
-        if op < 1:
-            print("Ingrese una opcion entre 1 y 4.")
-            continue
-    except ValueError:
-        print("Ingrese un numero valido.")
-        continue
-    break
+    if op == 1:
+        ingresar_usuario()
+    elif op == 2:
+        buscar_usuario()
+    elif op == 3:
+        eliminar_usuario()
+    elif op == 4:
+        print("Saliendo...")
+        break
+    else:
+        print("Ingrese una opcion valida entre 1 y 4.")
+
+                 
     
-
-if op == 1:
-    print("Ingresando Usuario...")
-    ingresar_usuario(usuarios)
-
-if op == 2:
-    print("Buscando Usuario...")
-
-if op == 3:
-    print("Eliminando Usuario...")
-
-if op == 4:
-    print("Saliendo.")
